0e23b6289a6fefad4fca7bab1b3588a78a53d154
leeool <lgonsalez02@gmail.com>
2022-04-08 22:34:29 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string id;
+double altura;
+
+Console.Write("Entre com seu sexo (F)/(M)..: ");
+id = Console.ReadLine()!;
+
+if (id == "M")
+{
+    Console.Write("Digite sua altura em (m)....: ");
+    altura = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"\nSeu peso ideal é {altura * 72.7 - 58:N1}kg");
+}
+
+else if (id == "F")
+{
+    Console.Write("Digite sua altura em (m)....: ");
+    altura = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"\nSeu peso ideal é {altura * 62.1 - 44.7:N1}kg");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("\nSEXO NÃO IDENTIFICADO.");
+}
+
+Console.ResetColor();
